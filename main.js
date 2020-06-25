5aaadc49ab1ed0f66b111597fe0fce4937835f26
Kruzchy Klaperman <focus16gfx@gmail.com>
2020-06-25 18:30:55 +0530

    takes an array as input

--- main.js
+++ main.js
@@ -5,14 +5,14 @@ const readline = require('readline');
 const { prompt } = require('enquirer');
 const cheerio = require('cheerio');
 
-
-const getDocx = async () => {
-    const response = await prompt({
-        type: 'input',
-        name: 'url',
-        message: 'Paste page URL'
-    });
-    const url = response.url
+const getDocx = async (url) => {
+    // const response = await prompt({
+    //     type: 'input',
+    //     name: 'url',
+    //     message: 'Paste page URL'
+    // });
+    // const url = response.url
+    console.log(`>>Processing ${url}`)
     let tmpUrlSplitArray = url.split('/')
     const filePath = `./documents/${tmpUrlSplitArray[tmpUrlSplitArray.length-1]}.docx`
 
@@ -34,15 +34,17 @@ const getDocx = async () => {
     $('.Hotels-Cityguides-CityGuidesHowToGet .airport-item').each((i, item) => {
         // const airportCode = $(this).find('div>span').text;
         // const airportName = $(this).find('div>a>span').text;
-
+        //CHEERIO HAS DECODING PROBLEMS. DO NOT USE EXTERNAL LIBRARY TO DECODE URL ENCODED STRINGS
         const airportCode = $('div>span', item).text();
         const airportName = $('div>a>span', item).text();
         airportCheerioObject.append(`<p class="xsxsxs">(${airportCode}) ${airportName}</p>`);
     });
     const fileBuffer = await HTMLtoDOCX($.html());
     fs.writeFileSync(filePath, fileBuffer);
-    console.log('DONE!')
 };
 
-
-getDocx();
+const inputData = fs.readFileSync('./input.txt', 'utf8').replace(/\r/g, '')
+const urlsArray = inputData.split('\n').filter(item=>!!item)
+console.log(urlsArray)
+urlsArray.forEach(url=>getDocx(url).catch(err=>console.error(err)));
+console.log('DONE!')
